a8be06665f18f9b8048283af4767c45e1a5526e4
Poorvi-R-Bhat <poorvi.r.bhat@gmail.com>
2024-05-16 03:02:48 +0000

    feat:ERPNext Price Estimate

--- erpnext_price_estimation/erpnext_price_estimation/doctype/partner_lead_lost_reason/test_partner_lead_lost_reason.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, frappe solutions and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestPartnerLeadLostReason(FrappeTestCase):
-	pass
--- erpnext_price_estimation/erpnext_price_estimation/doctype/partner_lead_type/partner_lead_type.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, frappe solutions and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Partner Lead Type", {
-// 	refresh(frm) {
-
-// 	},
-// });

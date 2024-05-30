a16f4ebce0bab0b34540fbb935726c6604dfecaa
Poorvi-R-Bhat <poorvi.r.bhat@gmail.com>
2024-05-30 12:20:40 +0530

    updated the docTypes

--- erpnext_price_estimation/erpnext_price_estimation/doctype/hosting/hosting.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, frappe solutions and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Hosting", {
-// 	refresh(frm) {
-
-// 	},
-// });
